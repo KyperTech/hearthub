523e5cf429aa86b72040e791017a0d8cd1891e39
prescottprue <prescottprue@hotmail.com>
2014-04-11 01:53:11 -0400

    Added gruntfile, ignore file, and package file.

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function(grunt) {
+    grunt.registerTask('watch', ['watch']);
+    grunt.registerTask('dist', ['shell:home']);
+    grunt.registerTask('test', ['shell:test']);
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //Trying for storing variables else where,
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    //keepalive: true, keeping grunt running
+                    //livereload:true,
+                    base: './src/',
+                    open: {
+                        target: 'http://localhost:8080',
+                        appName: 'Google Chrome',
+                    }
+                }
+            }
+        },
+        less: {
+            style: {
+                files: {
+                    "src/css/styles.css": "./less/styles.less"
+                },
+            }
+        },
+        watch: {
+            less: {
+                files: ["./less/*.less"],
+                tasks: ["less:style"],
+                options:{
+                    //debounceDelay:1000,
+                    livereload: true
+                }
+            },
+            html: {
+                files: ['src/index.html'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        shell:{
+            home:{
+                command:"s3cmd sync --exclude './src/bower_components/' --exclude '*.less' --exclude './src/less/' --exclude '.DS_Store' --exclude '.buildignore' --exclude '.htaccess' --acl-public ./src/ s3://hearthub-home"
+            },
+            test:{
+                command:"s3cmd sync --exclude './src/bower_components/' --exclude '*.less' --exclude './src/less/' --exclude '.DS_Store' --exclude '.buildignore' --exclude '.htaccess' --acl-public ./src/ s3://hearthub-test"
+            },
+            options:{
+                stdout:true
+            }
+        },
+        open:{
+          cat:{
+          path:"https://trigger.io/catalyst/client/#EA3E3BBD-C54D-4ECA-8FA9-6764B62E84F0",
+          app: "Google Chrome"
+          }
+        }
+
+    });
+
+    //Plugin for "watch"
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    //Connect plugin
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    //Open plugin
+    grunt.loadNpmTasks('grunt-open');
+
+    //Watch files for reload
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    //Less Plugin
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    //Grunt-Shell Plugin (for creating and pushing to production)
+    grunt.loadNpmTasks('grunt-shell');
+
+    // Default task(s).
+    grunt.registerTask('default', ['connect', 'less', 'watch']);
+
+    //Forge Tasks
+    grunt.registerTask('forge', ['open', 'less', 'watch']);
+
+    grunt.registerTask('serve', ['connect'], function() {
+        grunt.task.run('connect');
+    });
+};
